d71d73a28473ffc7395c65bff219a61105c123f3
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-11-14 14:24:48 +0200

    init RESTful api

---
+++ src/cmd/media-content-api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/5aradise/media-content-api/src/internal"
+
+func main() {
+	internal.Init()
+}
---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/5aradise/media-content-api/src
+
+go 1.23.2
