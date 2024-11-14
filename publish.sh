dea1fda7f0c7f89efe7964ab1fb16ed897186b99
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-11-14 15:21:49 +0200

    fix init configuration

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/5aradise/media-content-api master:gh-pages
+git push -f https://github.com/5aradise/media-content-api main:gh-pages
 
 cd -
